9f3b8e682a5545ea365849f38b9bcf0f1c274353
ellisaveta <elina123456@mail.bg>
2020-12-29 14:01:41 +0200

    Now Shunting-yard algorithm works

--- Project.cpp
+++ Project.cpp
@@ -16,27 +16,26 @@ int main()
 }
 void ShuntingYard(string input)
 {
-	vector<char> numbers;
+	vector<char> shYardString;
 	vector<char> operators;
 	vector<int> currentNumber;
 	char lastElementInOperators;
 	for (int i = 0; i < input.length(); ++i)
 	{
-		//while (isNumber(input[i]) && i<input.length())
-		//{
-		//	currentNumber.push_back(input[i]-48);
-		//	++i;
-		//}
-		if (isNumber(input[i]))
+		if (isNumber(input[i])) 
 		{
-			numbers.push_back(input[i]);
+			do
+			{
+				shYardString.push_back(input[i]);
+				++i;
+			} while ((isNumber(input[i])) && i < input.length());
+			shYardString.push_back(' ');
+			--i;
 			continue;
 		}
-		//currentNumber.clear();
-		//if (i == input.length()) break;
 		if (input[i] == '^')
 		{
-			numbers.push_back(input[i]);
+			operators.push_back(input[i]);
 			continue;
 		}
 		if (isOperator(input[i]))
@@ -45,10 +44,11 @@ void ShuntingYard(string input)
 			if (operators.size() > 0)
 			{
 				char o2 = operators[operators.size() - 1];
-				while (isOperator(o2) && comparePrecendence(o1, o2))
+				while (isOperator(o2) && comparePrecendence(o2, o1))
 				{
 					operators.pop_back();
-					numbers.push_back(o2);
+					shYardString.push_back(o2);
+					shYardString.push_back(' ');
 					if (operators.size() > 0) o2 = operators[operators.size()-1];
 					else break;
 				}
@@ -67,24 +67,26 @@ void ShuntingYard(string input)
 			while (lastElementInOperators != '(')
 			{
 				char op = lastElementInOperators;
-				numbers.push_back(op);
+				shYardString.push_back(op);
+				shYardString.push_back(' ');
 				operators.pop_back();
-				if(operators.size()>0) lastElementInOperators = operators[operators.size() - 1];
+				if (operators.size() > 0) lastElementInOperators = operators[operators.size() - 1];
 				else
 				{
 					cout << "NaN";
 					return;
 				}
-			}	
-		}
-		lastElementInOperators = operators[operators.size() - 1];
-		if (lastElementInOperators == '(') operators.pop_back();
-		lastElementInOperators = operators[operators.size() - 1];
-		if (lastElementInOperators == '^')
-		{
-			char op = lastElementInOperators;
-			numbers.push_back(op);
-			operators.pop_back();
+			}
+			if (operators.size() > 0) lastElementInOperators = operators[operators.size() - 1];
+			if (lastElementInOperators == '(') operators.pop_back();
+			if (operators.size() > 0) lastElementInOperators = operators[operators.size() - 1];
+			if (lastElementInOperators == '^')
+			{
+				char op = lastElementInOperators;
+				shYardString.push_back(op);
+				shYardString.push_back(' ');
+				operators.pop_back();
+			}
 		}
 	}
 	while (operators.size() > 0)
@@ -96,12 +98,13 @@ void ShuntingYard(string input)
 			return;
 		}
 		char op = lastElementInOperators;
-		numbers.push_back(op);
+		shYardString.push_back(op);
+		shYardString.push_back(' ');
 		operators.pop_back();
 	}
-	for (int i = 0; i < numbers.size(); ++i)
+	for (int i = 0; i < shYardString.size(); ++i)
 	{
-		cout << numbers[i];
+		cout << shYardString[i];
 	}
 }
 int TransformIntoNumber(vector<int> currentNumber)

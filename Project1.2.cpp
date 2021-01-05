ce4b9ceefd328cef7e21bffbb173f146781dde52
ellisaveta <elina123456@mail.bg>
2021-01-05 11:30:02 +0200

    Some fuctions are separated in AdditionalFunctions file

--- Project1.2.cpp
+++ Project1.2.cpp
@@ -17,14 +17,11 @@
 #include <vector>
 #include <fstream>
 #include <cmath>
+#include "AdditionalFunctions.h"
 using namespace std;
-int TransformIntoNumber(const vector<int>);
-void Calculator(string);
-bool Validation(string&);
-bool IsNumber(const char);
-bool IsOperator(const char);
-bool ComparePrecendence(const char, const char);
-double Calculate(const double, const double, const char, bool&);
+
+void Calculator(const string&);
+
 int main()
 {
 	string input;
@@ -45,46 +42,7 @@ int main()
 	return 0;
 }
 
-bool Validation(string& input)
-{
-	if (input.empty()) return false;
-	for (int i = 0; i < input.length(); ++i)
-	{
-		if (input[i] == '/')
-		{
-			++i;
-			while (i < input.length() && input[i] == ' ')
-			{
-				++i;
-			}
-			if (i < input.length())
-			{
-				if (input[i] == '0') return false;
-				--i;
-			}
-			else return false;
-		}
-		else if (input[i] == '-')
-		{
-			int j = i - 1;
-			while (j >= 0 && input[j] == ' ') --j;
-			if (j >= 0 && input[j] == '(')
-			{
-				input.insert(j+1, "0");
-				++i;
-			}
-			else if (j < 0)
-			{
-				input.insert(0, "0");
-				++i;
-			}
-		}
-		else if (IsNumber(input[i])==false && IsOperator(input[i])==false && input[i]!=' ' && input[i]!='(' && input[i]!=')') return false;
-	}
-	return true;
-}
-
-void Calculator(string input)
+void Calculator(const string& input)
 {
 	//Shunting-Yard algorithm...
 	vector<char> shYardString;
@@ -212,72 +170,4 @@ void Calculator(string input)
 		cout << "NaN"; //Too many numbers
 		return;
 	}
-}
-
-int TransformIntoNumber(const vector<int> currentNumber)
-{
-	int number = 0;
-	for(auto& i : currentNumber)
-	{
-		number *= 10;
-		number += i;
-	}
-	return number;
-}
-
-bool IsNumber(const char symbol)
-{
-	return (symbol > 47 && symbol < 58);
-}
-
-bool IsOperator(const char symbol)
-{
-	return (symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/' || symbol == '^');
-}
-
-bool ComparePrecendence(const char first, const char sec)
-{
-	if (sec == first) return true;
-	switch (first)
-	{
-		case '+':
-			if (sec == '-') return true;
-			return false;
-		case '-':
-			if (sec == '+') return true;
-			return false;
-		case '*':
-			if (sec == '+' || sec == '-' || sec == '/') return true;
-			return false;
-		case '/':
-			if (sec == '+' || sec == '-' || sec == '*') return true;
-			return false;
-		case '^':
-			return true;
-	}
-	return false;
-}
-
-double Calculate(const double a, const double b, const char symbol, bool& correct)
-{
-	switch (symbol)
-	{
-	case '+':
-		return a + b;
-	case '-':
-		return a - b;
-	case '*':
-		return a * b;
-	case '/':
-		if (b == 0)
-		{
-			correct = false;
-			return -1;
-		}
-		return a / b;
-	case '^':
-		return pow(a, b);
-	}
-	correct = false;
-	return -1;
 }
